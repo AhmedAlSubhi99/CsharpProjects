5b0ed9b7d4ffb640488d50851a1315bc07f86e37
Ahmed AlSubhi <cheer.codeline@gmail.com>
2025-03-16 11:42:29 +0400

    .

--- Program.cs
+++ Program.cs
@@ -599,18 +599,18 @@ namespace CsharpProjects
             //        ****
             //        *****
 
-            int N;
-            Console.WriteLine("Enter The N");
-            N = int.Parse(Console.ReadLine());
-            Console.WriteLine("The Pattern is ");
-            for (int i = 1; i <= N; i++)
-            {
-                for (int j = 1; j <= i; j++)
-                {
-                    Console.Write("*");
-                }
-                Console.WriteLine();
-            }
+            //int N;
+            //Console.WriteLine("Enter The N");
+            //N = int.Parse(Console.ReadLine());
+            //Console.WriteLine("The Pattern is ");
+            //for (int i = 1; i <= N; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
 
 
 
@@ -682,30 +682,30 @@ namespace CsharpProjects
             //Console.WriteLine("The Pattern is ");
             //if (N % 2 == 0)
             //{
-            //for (int i = 1; i <= N; i++)
-            //{
-            //    for (int j = 1; j <= N - i; j++)
+            //    for (int i = 1; i <= N; i++)
             //    {
-            //        Console.Write(" ");
+            //        for (int j = 1; j <= N - i; j++)
+            //        {
+            //            Console.Write(" ");
+            //        }
+            //        for (int k = 1; k <= 2 * i - 1; k++)
+            //        {
+            //            Console.Write("*");
+            //        }
+            //        Console.WriteLine();
             //    }
-            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    for (int i = N - 1; i >= 1; i--)
             //    {
-            //        Console.Write("*");
+            //        for (int j = 1; j <= N - i; j++)
+            //        {
+            //            Console.Write(" ");
+            //        }
+            //        for (int k = 1; k <= 2 * i - 1; k++)
+            //        {
+            //            Console.Write("*");
+            //        }
+            //        Console.WriteLine();
             //    }
-            //    Console.WriteLine();
-            //}
-            //for (int i = N - 1; i >= 1; i--)
-            //{
-            //    for (int j = 1; j <= N - i; j++)
-            //    {
-            //        Console.Write(" ");
-            //    }
-            //    for (int k = 1; k <= 2 * i - 1; k++)
-            //    {
-            //        Console.Write("*");
-            //    }
-            //    Console.WriteLine();
-            //}
             //}
             //else
             //{
